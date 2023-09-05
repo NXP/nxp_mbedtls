9329c691eadf8dc896183f80df10d2e08d423499
Ruchika Gupta <ruchika.gupta_1@nxp.com>
2023-09-05 15:04:46 +0530

    example: Add example to test key generation
    Example to generate key at a prticular location and test it's
    usage.
    
    The location of the key would need to be defined in .c and .function
    file for a new crypto driver. README has the details.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta_1@nxp.com>

---
+++ example/mbedtls3x_testsuite_generate/platform_alt.h
@@ -0,0 +1,20 @@
+#ifndef MBEDTLS_PLATFORM_ALT_H
+#define MBEDTLS_PLATFORM_ALT_H
+
+#include "mbedtls/build_info.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ff.h"
+#include "app.h"
+
+#define stderr          0
+typedef struct mbedtls_platform_context {
+    char dummy; /**< A placeholder member, as empty structs are not portable. */
+}
+mbedtls_platform_context;
+
+
+#endif /* platform.h */
