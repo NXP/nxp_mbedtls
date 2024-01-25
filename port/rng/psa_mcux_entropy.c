6299ce17674bd355ea8d6da2cfe08cf8154d06f6
Vit Chmelar <vit.chmelar@nxp.com>
2024-01-25 09:40:12 +0100

    [MCUX-65973] Reworked port of mbedtls_hardware_poll function for S200 to make it more clearer

--- port/rng/psa_mcux_entropy.c
+++ port/rng/psa_mcux_entropy.c
@@ -107,16 +107,34 @@ int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t 
     sss_sscp_rng_t ctx;
 
     result = CRYPTO_InitHardware();
-    if (sss_sscp_rng_context_init(&g_sssSession, &ctx, 0u) != kStatus_SSS_Success) 
+    if (result != kStatus_Success) 
     {
-    } else if (sss_sscp_rng_get_random(&ctx, output, len) != kStatus_SSS_Success) 
-    {
-    } else if (sss_sscp_rng_free(&ctx) != kStatus_SSS_Success) 
-    {
-    } else 
-    {
-        result = kStatus_Success;
+      return result;
     }
+    
+    result = sss_sscp_rng_context_init(&g_sssSession, &ctx, 0u);
+    if (result != kStatus_SSS_Success) 
+    {
+      return result;
+    }
+    
+    result = sss_sscp_rng_get_random(&ctx, output, len);
+    if (result != kStatus_SSS_Success) 
+    {
+      return result;
+    }
+    
+    result = sss_sscp_rng_free(&ctx);
+    if (result != kStatus_SSS_Success) 
+    {
+      return result;
+    }
+    
+    if (result == kStatus_SSS_Success) 
+    {
+      result = kStatus_Success;
+    }
+    
 #endif
 
     if (result == kStatus_Success)
