02d09c98d0ec0faee5d545655982f29062133f50
VitSolcikNXP <vit.solcik@nxp.com>
2025-11-11 10:09:17 +0100

    Revert "Allow gcc-15 to be in $PATH"
    This reverts commit b3721059394fccf3363c37ffd5c070ce760616cd.
    
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- tests/scripts/components-compiler.sh
+++ tests/scripts/components-compiler.sh
@@ -80,13 +80,7 @@ support_test_gcc_latest_opt () {
 # For the time being, we don't make $GCC_LATEST be GCC 15 on the CI
 # platform, because that would break branches where #9814 isn'f fixed yet.
 support_test_gcc15_drivers_opt () {
-    if type gcc-15 >/dev/null 2>/dev/null; then
-        GCC_15=gcc-15
-    elif [ -x /usr/local/gcc-15/bin/gcc-15 ]; then
-        GCC_15=/usr/local/gcc-15/bin/gcc-15
-    else
-        return 1
-    fi
+    test -x /usr/local/gcc-15/bin/gcc-15
 }
 component_test_gcc15_drivers_opt () {
     msg "build: GCC 15: full + test drivers dispatching to builtins"
@@ -101,7 +95,7 @@ component_test_gcc15_drivers_opt () {
     # Also disable a warning that we don't yet comply to.
     loc_cflags="${loc_cflags} -Wno-error=unterminated-string-initialization"
 
-    make CC=$GCC_15 CFLAGS="${loc_cflags}" LDFLAGS="$ASAN_CFLAGS"
+    make CC=/usr/local/gcc-15/bin/gcc-15 CFLAGS="${loc_cflags}" LDFLAGS="$ASAN_CFLAGS"
 
     msg "test: GCC 15: full + test drivers dispatching to builtins"
     make test
