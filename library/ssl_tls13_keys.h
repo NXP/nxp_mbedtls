5dc84e1cea880073b1f3fbad874b7507d294945a
VitSolcikNXP <vit.solcik@nxp.com>
2025-11-11 10:09:15 +0100

    Revert "Add __attribute__ ((nonstring)) to remove unterminated-string-initialization warning"
    This reverts commit 209cedd13a2590f026a73c0c1ebbeb5134bb56fe.
    
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- library/ssl_tls13_keys.h
+++ library/ssl_tls13_keys.h
@@ -40,9 +40,8 @@
 
 #if defined(MBEDTLS_SSL_PROTO_TLS1_3)
 
-/* We need to tell the compiler that we meant to leave out the null character. */
 #define MBEDTLS_SSL_TLS1_3_LABEL(name, string)       \
-    const unsigned char name    [sizeof(string) - 1] __attribute__ ((nonstring));
+    const unsigned char name    [sizeof(string) - 1];
 
 union mbedtls_ssl_tls13_labels_union {
     MBEDTLS_SSL_TLS1_3_LABEL_LIST
