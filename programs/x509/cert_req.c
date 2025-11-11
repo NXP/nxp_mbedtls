c821b3c03d2d323ef483143e3e0d68b7fa45e164
VitSolcikNXP <vit.solcik@nxp.com>
2025-11-11 10:09:13 +0100

    Revert "Remove redundant free loop"
    This reverts commit 6369ecc60dfc1aba5e70d5828f9d5fba9d2ccc9f.
    
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- programs/x509/cert_req.c
+++ programs/x509/cert_req.c
@@ -497,14 +497,6 @@ exit:
 #endif
     }
 
-    mbedtls_x509write_csr_free(&req);
-    mbedtls_pk_free(&key);
-    mbedtls_ctr_drbg_free(&ctr_drbg);
-    mbedtls_entropy_free(&entropy);
-#if defined(MBEDTLS_USE_PSA_CRYPTO)
-    mbedtls_psa_crypto_free();
-#endif /* MBEDTLS_USE_PSA_CRYPTO */
-
     cur = opt.san_list;
     while (cur != NULL) {
         mbedtls_x509_san_list *next = cur->next;
@@ -521,6 +513,22 @@ exit:
         mbedtls_free(cur);
         cur = next;
     }
+
+    mbedtls_x509write_csr_free(&req);
+    mbedtls_pk_free(&key);
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
+#if defined(MBEDTLS_USE_PSA_CRYPTO)
+    mbedtls_psa_crypto_free();
+#endif /* MBEDTLS_USE_PSA_CRYPTO */
+
+    cur = opt.san_list;
+    while (cur != NULL) {
+        prev = cur;
+        cur = cur->next;
+        mbedtls_free(prev);
+    }
+
 
     mbedtls_exit(exit_code);
 }
