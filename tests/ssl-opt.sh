99e8bde3719f33e5b39c9daa90c31904075a17a3
VitSolcikNXP <vit.solcik@nxp.com>
2025-11-11 10:09:19 +0100

    Revert "Print names of new tests properly"
    This reverts commit 1935d401fc17981f3dc2f79fb4bac9f7f69ba495.
    
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -2001,13 +2001,7 @@ run_test_export_keying_material() {
     unset EXPORTED_KEY1
     unset EXPORTED_KEY2
     TLS_VERSION="$1"
-
-    case $TLS_VERSION in
-        tls12) TLS_VERSION_PRINT="TLS 1.2";;
-        tls13) TLS_VERSION_PRINT="TLS 1.3";;
-    esac
-
-    run_test    "$TLS_VERSION_PRINT: Export keying material" \
+    run_test    "TLS $TLS_VERSION: Export keying material" \
                 "$P_SRV debug_level=4 force_version=$TLS_VERSION exp_label=test-label" \
                 "$P_CLI debug_level=4 force_version=$TLS_VERSION exp_label=test-label" \
                 0 \
@@ -2021,13 +2015,7 @@ run_test_export_keying_material_openssl_compat() {
     unset EXPORTED_KEY1
     unset EXPORTED_KEY2
     TLS_VERSION="$1"
-
-    case TLS_VERSION in
-        tls12) TLS_VERSION_PRINT="TLS 1.2";;
-        tls13) TLS_VERSION_PRINT="TLS 1.3";;
-    esac
-
-    run_test    "$TLS_VERSION_PRINT: Export keying material (OpenSSL compatibility)" \
+    run_test    "TLS $TLS_VERSION: Export keying material (OpenSSL compatibility)" \
                 "$P_SRV debug_level=4 force_version=$TLS_VERSION exp_label=test-label" \
                 "$O_CLI -keymatexport=test-label" \
                 0 \
